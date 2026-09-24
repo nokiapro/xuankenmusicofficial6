31dc495f2ee94dd57891235b3e2d68bbafe09867
nokiapro <xuankensinger@gmail.com>
2026-09-24 13:23:11 +0700

    Add files via upload

--- js/music.js
+++ js/music.js
@@ -2389,7 +2389,7 @@ function toggleTimerModal() {
         timerOverlay.classList.remove('show');
         setTimeout(() => {
             if (!timerModal.classList.contains('show')) timerOverlay.style.display = 'none';
-        }, 300);
+        }, 280);
     } else {
         timerOverlay.style.display = 'block';
         void timerOverlay.offsetHeight;
